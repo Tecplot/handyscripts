6583ce9bad9d679ff998302f63cad17e5092afc6
brandonmarkham <55457608+brandonmarkham@users.noreply.github.com>
2021-06-28 20:48:41 -0700

    Updating ParallelImageCreator.p and VerticalProfile.py
    Uploading documentations for Parallel ImageCreator.py and also JohnC's doc updates from on VerticalProfile.py
    
    Co-Authored-By: JohnConnell <38988020+connellrjohn@users.noreply.github.com>

--- python/ParallelImageCreator.py
+++ python/ParallelImageCreator.py
@@ -1,6 +1,37 @@
+"""
+This script runs in batch mode and exports images in parallel.
+
+Additional Python modules needed:
+---------------
+multiprocessing
+    A package that supports spawning processes
+atexit
+    A module that defines functions that register and unregister cleanup functions
+
+
+Example usage:
+--------------
+cd to the location of the .py script, and then run the following, on Windows (see PyTecplot batch install notes for Linux/macOS):
+
+    python ParallelImageCreator.py vortex_shedding_example.lay -w 500 -ss 3 -b vortex_example
+
+
+Example instructions to create and setup the Vortex Shedding .lay from example directory:
+1. In the Tecplot GUI, open VortexShedding.plt found in the examples directory of the Tecplot installation folder.
+2. Then, load the VortexShedding.sty.
+3. Save the layout as vortex_shedding_example.lay in the same location as  VortexShedding.plt.
+4. Move vortex_shedding_example.lay and VortexShedding.plt to the directory where you saved this .py script.
+
+
+Note:
+This script also uses -np, which is the number of concurrent processes to run
+By default, it is int(multiprocessing.cpu_count()/2)
+
+"""
 import multiprocessing
 import atexit
 import tecplot as tp
+
 
 
 def initialize_process(layout_file):
@@ -8,36 +39,36 @@ def initialize_process(layout_file):
     # Must register stop at exit to ensure Tecplot cleans
     # up all temporary files and does not create a core dump
     atexit.register(tp.session.stop)
-    
+
     # Set the Load-On-Demand strategy to minimize memory use to keep the RAM low
     tp.macro.execute_command("$!FileConfig LoadOnDemand { UNLOADSTRATEGY = MinimizeMemoryUse }")
-    
+
     tp.load_layout(layout_file)
-    
-    
+
+
 def save_image(args):
     solution_time, width, supersample, image_file = args
-    
+
     tp.active_frame().plot().solution_time = solution_time
     tp.export.save_png(image_file, width=width, supersample=supersample)
     return image_file
 
-    
+
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description="Process layout in parallel to create images.")
     parser.add_argument("layoutfile", help="Tecplot layout file to open")
-    parser.add_argument("-numprocs", help="Number of concurrent processes to run", 
-        type=int, default=int(multiprocessing.cpu_count()/2), )
-    parser.add_argument("-imagewidth", help="Width in pixels of the image",
-        type=int, default=1024)
-    parser.add_argument("-supersample", help="Supersample factor to use for image export",
-        type=int, default=2)
-    parser.add_argument("-imagebasename", help="Basename for exported PNG images",
-        default="image")
-    
+    parser.add_argument("-np", "--numprocs", help="Number of concurrent processes to run",
+                        type=int, default=int(multiprocessing.cpu_count()/2), )
+    parser.add_argument("-w", "--imagewidth", help="Width in pixels of the image",
+                        type=int, default=1024)
+    parser.add_argument("-ss", "--supersample", help="Supersample factor to use for image export",
+                        type=int, default=3)
+    parser.add_argument("-b", "--imagebasename", help="Basename for exported PNG images",
+                        default="image")
+
     args = parser.parse_args()
-    
+
     # Get the solution times over which to iterate. Stop PyTecplot in
     # the main process to free up the license for the workers. PyTecplot
     # cannot be restarted once stopped!
@@ -45,25 +76,25 @@ if __name__ == '__main__':
     tp.load_layout(args.layoutfile)
     solution_times = tp.active_frame().dataset.solution_times
     tp.session.stop()
-    
+
     # !!! IMPORTANT !!!
     # On Linux systems, Python's multiprocessing start method
     # defaults to "fork" which is incompatible with PyTecplot
     # and must be set to "spawn"
     multiprocessing.set_start_method('spawn')
-    
+
     # Set up the pool with initializing function and associated arguments
     pool = multiprocessing.Pool(processes=args.numprocs,
-                   initializer=initialize_process,
-                   initargs=(args.layoutfile,))
-                   
+                                initializer=initialize_process,
+                                initargs=(args.layoutfile,))
+
     try:
         job_args = []
-        for i,solution_time in enumerate(solution_times):
-            image_file = args.imagebasename + "%04d.png"%(i)
+        for i, solution_time in enumerate(solution_times):
+            image_file = args.imagebasename + "%04d.png" % (i)
             job_args.append((solution_time, args.imagewidth, args.supersample, image_file))
-        
-        image_files = pool.map(save_image, job_args)
+            image_files = pool.map(save_image, job_args)
+
     finally:
         # !!! IMPORTANT !!!
         # Must join the process pool before parent script exits
--- python/VerticalProfile.py
+++ python/VerticalProfile.py
@@ -1,13 +1,37 @@
+"""Create a vertical profile over time for the active contour of a 3D plot.
+
+usage:
+General:
+    > python VerticalProfile.py [args]
+
+usage example:
+    > python .\VerticalProfile.py -x 78.2 -y -50.66
+
+Necessary modules
+-----------------
+time
+    A module with generic time abilities
+tputils
+    Generic PyTecplot utilities supplied by the Tecplot GitHub handyscripts
+
+Description
+-----------
+Creates a single vertical profile for non time-dependent data. Creates a vertical profile over time for time-dependent data.
+Note the X and Y inputs are not necessarily the X and Y variables but rather the X and Y axis variables. If no args are supplied,
+the user will be prompted to add X and Y arguments. The Vertical Profile will be taken of the flood variable of the first fieldmap.
+See the source_fieldmap variable to change functionality.
+This script must be run in connected mode to function properly.
+
+"""
 import time
 import tecplot as tp
 from tecplot.constant import *
 import tputils
 
-tp.session.connect()
-
 
 def extract_vertical_line(x, y, source_fieldmap, plot):
-    tp.macro.execute_command("$!ActiveFieldMaps = [{}]".format(source_fieldmap))
+    '''Extracts a vertical line by creating a slice of a slice at the given X, Y locations.'''
+    tp.macro.execute_command("$!ActiveFieldMaps = [{}]".format(source_fieldmap + 1))
     x_slice = tp.data.extract.extract_slice(
         origin=(x, 0, 0),
         normal=(1, 0, 0),
@@ -22,6 +46,7 @@ def extract_vertical_line(x, y, source_fieldmap, plot):
 
 
 def plot_vertical_profile(zone, xvar, yvar):
+    '''Plots the vertical profile on a separate frame.'''
     frame = tp.active_page().add_frame()
     tputils.attach_dataset(frame, zone.dataset)
     plot = frame.plot(PlotType.Cartesian2D)
@@ -51,32 +76,56 @@ def plot_vertical_profile(zone, xvar, yvar):
     tp.macro.execute_command('''$!PropagateLinking
         LinkType = BetweenFrames
         FrameCollection = All''')
+    # Uncomment below to force tiled frames. Otherwise the new frame will overlay the 3D plot.
     # tp.macro.execute_extended_command(command_processor_id='Multi Frame Manager',
-    #    command='TILEFRAMESVERT')
+    #                                   command='TILEFRAMESVERT')
 
 
-frame = tp.active_frame()
-threed_plot = tp.active_frame().plot(PlotType.Cartesian3D)
-dataset = tp.active_frame().dataset
-active_fieldmap_indices = threed_plot.active_fieldmap_indices
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description="Create a Vertical Profile of an active Tecplot Session")
+    parser.add_argument("-x", help="X location of the vertical profile", type=float)
+    parser.add_argument("-y", help="Y location of the vertical profile", type=float)
+    args = parser.parse_args()
 
-source_fieldmap = 1
-new_strand = tputils.max_strand(dataset) + 1
+    tp.session.connect()
+    frame = tp.active_frame()
+    threed_plot = tp.active_frame().plot(PlotType.Cartesian3D)
+    threed_plot.show_contour = True
+    dataset = frame.dataset
+    active_fieldmap_indices = threed_plot.active_fieldmap_indices
+    source_fieldmap = 0
+    new_strand = tputils.max_strand(dataset) + 1
 
-x = float(input("Enter x location: "))
-y = float(input("Enter y location: "))
+    # Argument handling for X and Y variables. Prompts user if variables are not supplied.
+    if args.x is not None:
+        x = args.x
+    else:
+        x = float(input("Enter x location: "))
+    if args.y is not None:
+        y = args.y
+    else:
+        y = float(input("Enter y location: "))
 
-with tp.session.suspend():
-    for t in dataset.solution_times:
-        start = time.time()
-        threed_plot.solution_time = t
-        result = extract_vertical_line(x, y, source_fieldmap, threed_plot)
-        result.strand = new_strand
-        result.solution_time = t
-        print("Made vertical profile for t={} in {} seconds".format(t, time.time() - start))
+    with tp.session.suspend():
+        # Check if data is time-dependent
+        if dataset.solution_times:
+            # Data with solution times will create a vertical profile for each timestep.
+            for t in dataset.solution_times:
+                start = time.time()
+                threed_plot.solution_time = t
+                result = extract_vertical_line(x, y, source_fieldmap, threed_plot)
+                result.strand = new_strand
+                result.solution_time = t
+                print("Made vertical profile for t={} in {} seconds".format(t, time.time() - start))
+        else:
+            # Data with no solution times will still create a single vertical profile.
+            start = time.time()
+            result = extract_vertical_line(x, y, source_fieldmap, threed_plot)
+            print("Made a single vertical profile in {} seconds".format(time.time() - start))
 
-    threed_plot.active_fieldmap_indices = active_fieldmap_indices
+        threed_plot.active_fieldmap_indices = active_fieldmap_indices
 
-    xvar = threed_plot.fieldmap(source_fieldmap).contour.flood_contour_group.variable
-    yvar = threed_plot.axes.z_axis.variable
-    plot_vertical_profile(result, xvar, yvar)
+        xvar = threed_plot.fieldmap(source_fieldmap).contour.flood_contour_group.variable
+        yvar = threed_plot.axes.z_axis.variable
+        plot_vertical_profile(result, xvar, yvar)
