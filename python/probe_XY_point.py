ca6c11b8ce457b6cfc01eaaee8ac137a1cd4c4e6
Gibson Adams <gibson@Local-Admins-MacBook-Pro.local>
2024-01-18 13:33:50 -0800

    probe values of XY line plot using interpolation.

---
+++ python/probe_XY_point.py
@@ -0,0 +1,41 @@
+"""  
+    Takes value in range of x and outputs interpolated y-value. 
+    A way to probe for func values of XY plots.
+
+    Example below uses customlabels.plt datadset included with pytecplot and probes a value of the "Concentration" variable in XY plot. 
+"""
+import tecplot as tp
+from os import path
+from tecplot.constant import *
+from scipy.interpolate import interp1d
+
+tp.session.connect()
+
+#load data
+examples_dir = tp.session.tecplot_examples_directory()
+datafile = path.join(examples_dir, 'SimpleData', 'CustomLabels.plt') 
+dataset = tp.data.load_tecplot(datafile)
+
+# Select Variable by name or by zone number
+# remember tecplot is 1 indexed so 0 refers to the 1st tecplot zone
+variable = "Concentration"
+ds = tp.active_frame().dataset
+conc = ds.zone(variable)
+
+
+# use interpolation of X and Y axes to get value.
+def probe_func_at(value):
+    
+    # Uses default X and Y axes
+    x_array = conc.values(0)[:]
+    y_array = conc.values(1)[:]
+
+    if value > max(x_array) or value < min(x_array):
+        print("value not in domain of given x_array")
+        return
+        
+    out = interp1d(x_array, y_array, kind='linear')
+    print(f"At X = {value}, ", f"{variable} =", out(value))
+    return
+
+probe_func_at(11.33)
