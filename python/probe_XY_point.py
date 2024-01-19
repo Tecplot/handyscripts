ab2d0630d0d826d8b0459cdc386a13f906c4caea
Gibson Adams <gibson@Local-Admins-MacBook-Pro.local>
2024-01-19 12:34:23 -0800

    some changes to probe_XY_line script

--- python/probe_XY_point.py
+++ python/probe_XY_point.py
@@ -4,12 +4,14 @@
 
     Example below uses customlabels.plt datadset included with pytecplot and probes a value of the "Concentration" variable in XY plot. 
 """
+import sys
 import tecplot as tp
 from os import path
 from tecplot.constant import *
 from scipy.interpolate import interp1d
 
-tp.session.connect()
+if '-c' in sys.argv:
+	tp.session.connect()
 
 #load data
 examples_dir = tp.session.tecplot_examples_directory()
@@ -18,17 +20,17 @@ dataset = tp.data.load_tecplot(datafile)
 
 # Select Variable by name or by zone number
 # remember tecplot is 1 indexed so 0 refers to the 1st tecplot zone
-variable = "Concentration"
+zname = "Concentration"
 ds = tp.active_frame().dataset
-conc = ds.zone(variable)
+zn = ds.zone(zname)
 
 
 # use interpolation of X and Y axes to get value.
 def probe_func_at(value):
     
     # Uses default X and Y axes
-    x_array = conc.values(0)[:]
-    y_array = conc.values(1)[:]
+    x_array = zn.values(0)[:]
+    y_array = zn.values(1)[:]
 
     if value > max(x_array) or value < min(x_array):
         print("value not in domain of given x_array")
