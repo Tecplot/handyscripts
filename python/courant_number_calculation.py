5686af12738193d6934fa6a0167a333919f98f86
Gibson Adams <gibson@tecplot.com>
2025-01-14 09:25:32 -0800

    improvements to courant_number_calculation script

--- python/courant_number_calculation.py
+++ python/courant_number_calculation.py
@@ -8,7 +8,8 @@ and is linked to the Courant Friedrichs Lewy (CFL) stability condition of numeri
 
 source: https://sim-flow.com/courant-number-in-cfd/
 
-For 2D axis-aligned cases, below courant calculation will yield 0 due to dot product, seek "simple_courant_number_calculation.py" script
+For 2D axis-aligned cases, the below calculation will yield 0 in two out of three axes.
+This is expected, due to dot product in formula.
 
 We seek a general formulation of the Courant number to work in all structured CFD cases, 
 therefore the equation below:
@@ -23,9 +24,9 @@ Here, simple average is used.
 V - length/area/volume of cell.
 
 We expect the sum of all normals facing the cell centroid to be 0, 
-therefore the magnitude of the inner product of A * n is used.
+therefore the "magnitude" of the inner product of A * n is used.
 This results in double the desired result, 
-therefore we introduce the 1/2 constant.
+therefore 1/2 constant is introduced.
 """
 
 import math
@@ -35,12 +36,12 @@ from tecplot.constant import *
 import time
 
 # tp.data.load_tecplot('/path/to/dataset.plt')
-
 tp.session.connect()
 ds = tp.active_frame().dataset
 
 ZONE_NUMBER = 1
-VELOCITY_VAR_NAME = 'VelocityVec_X'
+VELOCITY_VAR_NAME = 'VelocityVec_Y'
+VELOCITY_COMPONENT = 'v'
 
 
 class DefineFEGeom:
@@ -245,7 +246,7 @@ class DefineFEGeom:
             return
 
 
-def get_cell_volume_var():
+def get_cell_volume_var(zone):
     """
     Checks for cell volume var.
     """
@@ -253,6 +254,11 @@ def get_cell_volume_var():
         print("calculating cell volume variable")
         tp.macro.execute_extended_command(command_processor_id='CFDAnalyzer4',
             command="Calculate Function='CELLVOLUME' Normalization='None' ValueLocation='Nodal' CalculateOnDemand='F' UseMorePointsForFEGradientCalculations='F'")
+    
+    cell_vol = zone.values('Cell Volume').as_numpy_array()
+    zeros_in_zone = np.any(cell_vol == 0)
+    if zeros_in_zone:
+        print("zone contains zeros for cell_volume, calculation will fail.")
     return
 
 
@@ -262,7 +268,11 @@ def get_representative_time_step():
     return (max(solution_times) - min(solution_times)) / len(solution_times)
 
 
-def get_all_vars_as_nodal(zone, velocity_var_name):
+def get_needed_vars(zone, velocity_var_name):
+    """
+    Gets needed variables from dataset as nodal. 
+    Resets the courant-number variables in given zone if exists.
+    """
     x = zone.values('X')
     y = zone.values('Y')
     z = zone.values('Z')
@@ -296,17 +306,26 @@ def get_all_vars_as_nodal(zone, velocity_var_name):
                                              value_location=tp.constant.ValueLocation.Nodal)
             
             var_set[var_name] = zone.values(f'{var_name}_Nodal')
-        
+    
+    # it is important that we initialize courant number as CC, 
+    # as we will iterate over elements and corresponding nodemaps.
+    if ds.variable('Courant number') or ds.variable('Courant number_Nodal'):
+        zone.values('courant number')[:] = [0 for _ in range(len(zone.values('Courant number')[:]))]
+        zone.values('courant number_Nodal')[:] = [0 for _ in range(len(zone.values('Courant number_Nodal')[:]))]
+    else:
+        if not ds.variable('Courant number'):
+            ds.add_variable('Courant number', locations=ValueLocation.CellCentered)
     return var_set
 
 
-def calculate_courant_number_for_zone(zone, velocity_var_name: str, manually_set_timestep=False):
+def calculate_courant_number_for_zone(zone_number, velocity_var_name: str, velocity_component, manually_set_timestep=False):
     """
     Params:
-    zone - supply zone number
+    zone_number - supply zone number you wish to do calculation on.
     velocity_var_name - give the name of your velocity variable in Tecplot
-    manually_set_timestep - enter timestep here if you wish to manually input a value, 
-    defaults to false. 
+    velocity_component - takes 'u', 'v', or 'w'. Tells the courant calculation how to set velocity vector.
+    manually_set_timestep - Enter timestep here if you wish to manually input a value, 
+    defaults to False.
     
     Overview:
     Given zonetype, i.e. FEBrick, we then need to define each face, 
@@ -323,75 +342,83 @@ def calculate_courant_number_for_zone(zone, velocity_var_name: str, manually_set
      | /        | /
     n1----------n4
     """
+    # Check timestep and zone information
+    zone = ds.zone(zone_number)
     if manually_set_timestep:
         timestep = manually_set_timestep
     else:
         timestep = get_representative_time_step()
-    
     if timestep == 0:
         print(f"timestep is {timestep} for simulation, courant number will be 0. Is this data transient?")
-        return
-        
     zone_type = zone.zone_type
-    get_cell_volume_var()
-    var_set = get_all_vars_as_nodal(zone, velocity_var_name)
+    get_cell_volume_var(zone)
+    var_set = get_needed_vars(zone, velocity_var_name)
     
-    print(f"number of elements: {zone.num_elements}")
-    start = time.time()
+    # intialize vars for calculation
     x = var_set['X'].as_numpy_array()
     y = var_set['Y'].as_numpy_array()
     z = var_set['Z'].as_numpy_array()
-    xyz_data = np.stack((x,y,z), axis=1)
-    u = var_set[velocity_var_name].as_numpy_array()
-    cell_vol = var_set['Cell Volume'].as_numpy_array()
+    xyz_data = np.stack((x,y,z), axis=1) # -> turns 3 searches into 1
+    velocity_array = var_set[velocity_var_name].as_numpy_array()
+    cell_vol_array = var_set['Cell Volume'].as_numpy_array()
+    courant_values = [0 for _ in range(len(zone.values('Courant number')[:]))]
 
-    if not list(ds.variables('Courant number')):
-        ds.add_variable('Courant number', locations=ValueLocation.Nodal)
-    zone.values('Courant number')[:] = [0 for _ in range(len(zone.values('Courant number')[:]))]
-    
     start = time.time()
     with tp.session.suspend():
         print("calculating Courant number on each element.")
         checkpoint_nums = [math.floor(zone.num_elements / 10) * i for i in range(0,9)]
-        for element in range(1350, zone.num_elements-63783):
-            original_element_num = element          
+        for element_num in range(zone.num_elements):
+            # We calculate courant num on each element, using its nodemap.
+            # We keep track of parent-zone element number AND section_element number.
+            parent_zone_element_num = element_num
             if zone_type == ZoneType.FEMixed:
-                # for FE-Mixed, retrieve the element number from the section which contains the element. 
-                # This is needed because the "element" number reverts back to 0 for new each nodemapsection.
-                nodemap_section = zone.nodemap.section_element(element)
-                element = nodemap_section[1]
-                nmap = zone.nodemap.nodes(element, section=nodemap_section[0])
+                nodemap_section = zone.nodemap.section_element(element_num)
+                element_num = nodemap_section[1]
+                nmap = zone.nodemap.nodes(element_num, section=nodemap_section[0])
             else:
                 # nodemap access is different for FEClassicNodemap object
-                nmap = np.array(zone.nodemap[element])
+                nmap = np.array(zone.nodemap[element_num])
+            
             nmap_coords = xyz_data[nmap]
-            u_node = u[nmap[0]]
-            node_cell_vol = cell_vol[nmap[0]]
+            velocity_at_node = velocity_array[nmap[0]]
+            cell_vol_at_node = cell_vol_array[nmap[0]]
                          
             cell_geom = DefineFEGeom(nmap_coords)
             area_vectors = cell_geom.get_geom_area_vectors(zone_type=zone_type)
+            courant_number_on_each_face = [calculate_courant_number(timestep, velocity_at_node, velocity_component, normal, area, cell_vol_at_node) for normal, area in area_vectors.values()]
+            total_courant_number = float(sum(courant_number_on_each_face))
+            courant_values[parent_zone_element_num] = total_courant_number
 
-            courant_number_on_each_face = [calculate_courant_number(timestep, u_node, normal, area, node_cell_vol) for normal, area in area_vectors.values()]
-            total_courant_number = sum(courant_number_on_each_face)
-            zone.values('Courant number')[nmap[0]] = total_courant_number
-            
-            if original_element_num in checkpoint_nums:
-                print(f"On cell {original_element_num + 1} out of ~{zone.num_elements}")
+            # time tracking
+            if parent_zone_element_num in checkpoint_nums:
+                print(f"On cell {parent_zone_element_num + 1} out of ~{zone.num_elements}")
                 print(f"time since calculation start: {round(time.time() - start, 2)}", "\n")
-            if original_element_num == zone.num_elements-1:
-                finish = time.time()
-                print(f"time taken: {round(finish - start, 2)}")
-
-    tp.session.clear_suspend()
-
-
-def calculate_courant_number(t, U, n, A, V):
-    U_vector = [U, 0, 0]
-    print(U_vector)
-    mag_U_dot_n = abs(np.dot(U_vector, n))
-    print(mag_U_dot_n)
-    return ((0.5)*t*mag_U_dot_n*A / V)
+        
+        finish = time.time()
+        print(f"time taken: {round(finish - start, 2)}")
+        # Finally, set courant number values and interpolate to nodal.
+        zone.values('Courant number')[:] = courant_values
+        tp.data.operate.execute_equation(f"{{Courant number_Nodal}} = {{courant number}}",
+                                        zones=zone_number,
+                                        value_location=ValueLocation.Nodal)
+        return
 
 
-calculate_courant_number_for_zone(ds.zone(ZONE_NUMBER), VELOCITY_VAR_NAME)
+def calculate_courant_number(t, velocity_var, velocity_component, n, A, V):
+    if velocity_component == 'u':
+        velocity_vector = [velocity_var, 0, 0]
+    elif velocity_component == 'v':
+        velocity_vector = [0, velocity_var, 0]
+    elif velocity_component == 'w':
+        velocity_vector = [0, 0, velocity_var]
+    else:
+        print(f"Cannot recognize velocity component: {velocity_component}, must use 'u', 'v' or 'w'.")
+    mag_velocity_dot_n = abs(np.dot(velocity_vector, n))
+    return ((0.5)*t*mag_velocity_dot_n*A / V)
+
+
+calculate_courant_number_for_zone(ZONE_NUMBER, 
+                                  VELOCITY_VAR_NAME, 
+                                  VELOCITY_COMPONENT, 
+                                  manually_set_timestep=False)
 
